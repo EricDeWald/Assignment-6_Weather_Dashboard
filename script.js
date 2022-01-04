46df34e5b66fed18cfe64f2b26e870662df8d403
EricDeWald <eric.l.dewald@gmail.com>
2022-01-03 21:26:24 -0600

    setting up the api calls
    added a function to get the data from the api and turn it into a json object

---
+++ script.js
@@ -0,0 +1,31 @@
+var search_data = document.querySelector('#city_search')
+var search_button = document.querySelector('#search_button')
+
+search_button.onclick= function(){
+    console.log('click')
+    console.log(search_data.value.trim())
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+search_data.value.trim()+'&appid=32f08ff1a5bc34c151e7de01ec1cf139').then(function(response){
+       return response.json()
+    }).then(function(daily_weather_json){
+        console.log(daily_weather_json)
+        search_data.value = ''
+        console.log(new Date().toLocaleDateString())
+        display_daily_weather(daily_weather_json)
+    })
+    
+};
+var display_daily_weather = function(weather_info){
+    var city_date = document.createElement("p")
+    city_date.textContent = weather_info.name + "("+new Date().toLocaleDateString() +")"
+    document.querySelector(".current_weather").appendChild(city_date) 
+};
+var total_number=0
+var average = function(list_of_nums){
+    for(i=0;i<list_of_nums.length;i++){
+        total_number+=list_of_nums[i]
+        
+    }
+    return total_number/list_of_nums.length
+};
+console.log(average([2,4]))
+console.log(total_number)
